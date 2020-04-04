f2ff9dc9354774bdf0a21a68fdd925429aec7bcd
Marc Hewison <Marc Hewison>
2020-04-04 15:37:33 +0100

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,63 @@
-# basic-validation
-Basic validation for Laravel models
+# Basic Validation
+Basic validation for Laravel models. On creation and update events by default.
+
+## Usage
+
+- if basic validation is required
+```php
+class LaravelModel {
+    use Hewison\BasicValidation;
+
+    public $rules = [
+        'column' => 'required'
+    ];
+
+    public $messages = [
+        'column.required' => 'This is a custom message'
+    ];
+}
+```
+
+- if more advanced rules are required a method is provided
+```php
+use Illuminate\Validation\Rule;
+
+class LaravelModel {
+    use Hewison\BasicValidation;
+
+    const AVAILABLE_OPTIONS = [
+        'option_1',
+        'option_2',
+    ];
+
+    public function validationRules()
+    {
+        return [
+            'column' => [
+                'required',
+                Rule::In(self::AVAILABLE_OPTIONS)
+            ],
+        ];
+    }
+}
+```
+
+- validation will fire when using any of the following methods
+```php
+    LaravelModel::create();
+    LaravelModel::update();
+    LaravelModel::save();
+```
+
+- if you require that validation does not fire for specific events you can use ```php disableValidation() ```
+
+```php
+    LaravelModel::find(1)->disableValidation()->update();
+    LaravelModel::find(1)->enableValidation()->update();
+```
+
+- You can also remove specific rules for an event, this will bypass validation for specific columns
+```php
+    LaravelModel::find(1)->removeRules('column')->update();
+    LaravelModel::find(1)->removeRules(['column_1', 'column_2'])->update();
+```
---
+++ src/BasicValidation.php
@@ -0,0 +1,187 @@
+<?php
+
+namespace Hewison;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Arr;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Validation\ValidationException;
+
+trait BasicValidation
+{
+    /**
+     * Laravel validator instance
+     * @var \Illuminate\Validation\Validator
+     */
+    private $validator;
+
+    /**
+     * Validation is enabled
+     */
+    protected $validation_enabled = true;
+
+    /**
+     * Laravel validation rules
+     * @var array
+     */
+    public $rules = [];
+
+    /**
+     * Laravel validation messages
+     * @var array
+     */
+    public $messages = [];
+
+    /**
+     * Trait boot method
+     */
+    public static function bootBasicValidation() : void
+    {
+        self::saving(function (Model $model) {
+            if ($model->isValid() !== true && $model->validation_enabled) {
+                throw new ValidationException($model->getValidator());
+            }
+        });
+    }
+
+    /**
+     * Check validity
+     * @return bool
+     * @throws \Exception
+     */
+    public function isValid() : bool
+    {
+        $this->resolveRules();
+
+        if (!$this->hasRules()) {
+            return true;
+        }
+
+        $validator = $this->makeValidator();
+
+        if ($validator->fails()) {
+            $this->validator = $validator;
+            return false;
+        }
+
+        return true;
+    }
+
+    /**
+     * Get the validator instance
+     * @return \Illuminate\Validation\Validator
+     */
+    protected function makeValidator() : \Illuminate\Validation\Validator
+    {
+        return Validator::make(
+            static::toArray(),
+            $this->rules,
+            $this->hasCustomMessages() ? $this->messages : []
+        );
+    }
+
+    /**
+     * Determine if a model has rules
+     * @return bool
+     */
+    protected function hasRules() : bool
+    {
+        return (bool) (is_array($this->rules) && count($this->rules));
+    }
+
+    /**
+     * Determines if a rules method is defined
+     * @return bool
+     */
+    private function hasValidationRulesMethod() : bool
+    {
+        return method_exists($this, 'validationRules');
+    }
+
+    /**
+     * Resolves the validation rules from the model
+     * @return void
+     */
+    private function resolveRules() : void
+    {
+        $this->rules = !$this->hasValidationRulesMethod() ? $this->rules : static::validationRules();
+    }
+
+    /**
+     * Informs if there are custom messages.
+     * @return bool
+     */
+    public function hasCustomMessages() : bool
+    {
+        return (bool) (is_array($this->messages) && count($this->messages));
+    }
+
+    /**
+     * Returns an array of validation errors
+     * @return \Illuminate\Validation\Validator
+     */
+    public function getValidator() : \Illuminate\Validation\Validator
+    {
+        return $this->validator;
+    }
+
+    /**
+     * Removes rules from the validator
+     * @param array|string $rule
+     * @return $this
+     */
+    public function removeRules($rule) : self
+    {
+        if (is_array($rule)) {
+            foreach($rule as $r) {
+                $this->removeRule($rule);
+            }
+        } else {
+            $this->removeRule($rule);
+        }
+
+        return $this;
+    }
+
+    /**
+     * Remove a specific rule if in rules array
+     * @param string $rule
+     */
+    private function removeRule(string $rule) : void
+    {
+        if ($this->hasRule((string) $rule)) {
+            Arr::forget($this->rules, $rule);
+        }
+    }
+
+    /**
+     * @param string $rule
+     * @return bool
+     */
+    private function hasRule(string $rule) : bool
+    {
+        return Arr::has($this->rules, $rule);
+    }
+
+    /**
+     * Allows inline disabling of validation
+     * @return $this
+     */
+    public function disableValidation() : self
+    {
+        $this->validation_enabled = false;
+
+        return $this;
+    }
+
+    /**
+     * Allows inline disabling of validation
+     * @return $this
+     */
+    public function enableValidation() : self
+    {
+        $this->validation_enabled = true;
+
+        return $this;
+    }
+}
